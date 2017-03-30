da61ea72f71c1e737968b6b3a056601a9be9206d
Zachariah Nagatani <zachnagatani@gmail.com>
2017-03-30 12:29:48 -0700

    Feat: Small and Medium Blog Cards Markup

--- templates/index.php
+++ templates/index.php
@@ -1,4 +1,61 @@
 <?php require_once('includes/header.php'); ?>
 
+        <main>
+            <div class="container">
+                <section class="posts-section">
+                    <div class="row">
+                        <div class="col-sm-8">
+                            <article class="blog-post blog-post--large">
+                                <h2>Why cats are the best!</h2>
+                            </article>
+
+                            <article class="blog-post blog-post--medium">
+                                <div class="blog-post--medium__img"
+                                     style="background: url('https://i.ytimg.com/vi/tntOCGkgt98/maxresdefault.jpg') no-repeat center center;
+                                     background-size: cover;">
+                                </div>
+                                <div class="blog-post--medium__content">
+                                    <h2 class="blog-post__heading">For real, cats are the best</h2>
+                                    <p class="blog-post__tagline">No doubt, my friend.</p>
+                                    <footer class="blog-post__footer">
+                                    <a href="#" class="blog-post__author">zachnagatani</a>
+                                    <small class="blog-post__timestamp">Mar 30</small>
+                                    </footer>
+                                </div>
+                            </article>
+                        </div>
+
+                        <div class="col-sm-4">
+                            <article class="blog-post blog-post--small">
+                                <h2 class="blog-post__heading">For real, cats are the best</h2>
+                                <p class="blog-post__tagline">No doubt, my friend.</p>
+                                <footer class="blog-post__footer">
+                                    <a href="#" class="blog-post__author">zachnagatani</a>
+                                    <small class="blog-post__timestamp">Mar 30</small>
+                                </footer>
+                            </article>
+
+                            <article class="blog-post blog-post--small">
+                                <h2 class="blog-post__heading">For real, cats are the best</h2>
+                                <p class="blog-post__tagline">No doubt, my friend.</p>
+                                <footer class="blog-post__footer">
+                                    <a href="#" class="blog-post__author">zachnagatani</a>
+                                    <small class="blog-post__timestamp">Mar 30</small>
+                                </footer>
+                            </article>
+
+                            <article class="blog-post blog-post--small">
+                                <h2 class="blog-post__heading">For real, cats are the best</h2>
+                                <p class="blog-post__tagline">No doubt, my friend.</p>
+                                <footer class="blog-post__footer">
+                                    <a href="#" class="blog-post__author">zachnagatani</a>
+                                    <small class="blog-post__timestamp">Mar 30</small>
+                                </footer>
+                            </article>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </main>
 
 <?php require_once('includes/footer.php'); ?>
